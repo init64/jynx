0ca6ad5b84dc5c91889ef4b1fbb87a31c9d377db
Alex Zolotarov <dxv1d@tuta.io>
2021-10-13 20:27:52 +0300

    added sass devDep, rewrited new Arch

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const socket = io();
+
+const botBar = document.querySelector('.botbar');
+const messages = document.querySelector('.messages');
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const username = document.querySelector('.username');
+
+username.innerHTML = prompt('Your name?')
+
+button.addEventListener('click', event => {
+    console.log('done');
+    event.preventDefault();
+    input.value ?
+        socket.emit('chatMessage', {
+            name: username.innerHTML,
+            message: input.value
+        }) : null;
+    input.value = '';
+})
+
+let logMessages = '';
+
+socket.on('chatMessage', data => {
+    logMessages += `<li><span>${data.name}:</span> ${data.message}</li>`;
+    messages.innerHTML = logMessages;
+})
