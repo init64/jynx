3ef6b62056d4c1a58b32b3d0953a5510deaffcff
bo-bic-laki <nikita134222@gmail.com>
2021-10-14 00:51:03 +0300

    Hotfixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,10 +5,14 @@ const app = new Vue({
     data: {
         login: false,
         user: {},
-        messages: [
-            { message: { content: '123321', id: 'test' }, user: { username: 'Heito' } },
-            { message: { content: '321123', id: 'test' }, user: { username: 'Heito' } }
-        ]
+        messages: [],
+        content: '',
+        platforms: {
+            Windows: 'uil uil-windows',
+            Linux: 'uil uil-linux',
+            Android: 'uil uil-android-alt',
+            iPhone: 'uil uil-apple-alt'
+        }
     },
     methods: {
         buttonLogin() {
@@ -17,7 +21,43 @@ const app = new Vue({
         },
         setColor(color) {
             socket.emit('user:update', { color })
-        }
+        },
+        sendMessage() {
+            if (this.content.trim() === '') return
+            socket.emit('chat:sendMessage', this.content)
+            return this.content = ''
+        },
+        unix(unix = Date.now()) {
+            const Months_name = [ "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" ];
+        
+            let date = new Date(unix),
+                year = date.getFullYear(),
+                day = date.getDate(),
+                month = date.getMonth(),
+                hours = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds();
+        
+            if (month < 10) month = `0${ month }`;
+            if (day < 10) day = `0${ day }`;
+            if (hours < 10) hours = `0${ hours }`;
+            if (hours >= 24) hours = `0${ hours - new Number(24) }`;
+            if (minutes < 10) minutes = `0${ minutes }`;
+            if (minutes >= 60) minutes = `0${ minutes - new Number(60) }`;
+            if (seconds < 10) seconds = `0${ seconds }`;
+            if (seconds >= 60) seconds = `0${ seconds - new Number(60) }`;
+        
+        
+            return {
+                year      : year,
+                day       : day,
+                month     : month,
+                month_name: Months_name[Number(month)],
+                hours     : hours,
+                minutes   : minutes,
+                seconds   : seconds
+            }
+        },
     },
     mounted() {
         socket.emit('user:connect', localStorage['userID'])
@@ -25,6 +65,16 @@ const app = new Vue({
         socket.on('user:loadUser', user => {
             this.user = user;
             localStorage.setItem('userID', user.id)
+            socket.emit('chat:getMessages')
+        })
+
+        socket.on('chat:loadMessages', messages => {
+            this.messages = messages;
+        })
+
+        socket.on('chat:addMessage', message => {
+            this.messages.push(message);
+            document.querySelector('.chat .list-messages').scrollTop = document.querySelector('.chat .list-messages').scrollHeight;
         })
     }
 })
--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.use('/', express.static(path.join(__dirname + '/dist')))
 const getUsers = () => JSON.parse(fs.readFileSync('./json/users.json', 'utf-8'));
 const loadUsers = data => fs.writeFile('./json/users.json', JSON.stringify(data), 'utf-8', err => err ? console.log(err) : '')
 
+const getMessages = () => JSON.parse(fs.readFileSync('./json/messages.json', 'utf-8'));
+const loadMessages = data => fs.writeFile('./json/messages.json', JSON.stringify(data), 'utf-8', err => err ? console.log(err) : '')
+
+const getPlatform = pf => {
+    if (/Windows/.test(pf)) return 'Windows'
+    if (/Android/.test(pf)) return 'Android'
+    if (/iPhone/.test(pf)) return 'iPhone'
+    if (/Linux/.test(pf)) return 'Linux'
+}
+
 io.on('connection', socket => {
 
     // * User Events
@@ -28,6 +38,7 @@ io.on('connection', socket => {
             users[id] = { id, username: '', color: '#fff', avatar: '' }
             loadUsers(users)
         }
+        users[id]['platform'] = getPlatform(socket.handshake.headers['user-agent'])
         socket.userID = id;
         socket.emit('user:loadUser', users[id])
     })
@@ -41,10 +52,26 @@ io.on('connection', socket => {
 
 
     // * Chat Events
-    socket.on('chatMessage', data => {
-        io.emit('chatMessage', {
-            name: data.name,
-            message: data.message
-        })
+    socket.on('chat:getMessages', () => {
+        let messages = getMessages(),
+            users = getUsers();
+        socket.emit('chat:loadMessages', messages.map(x => {
+            x.user = users[x.user]
+            return x
+        }))
+    })
+
+    socket.on('chat:sendMessage', message => {
+        let data = {
+            content: message,
+            id: generat({ type: 'All', length: 18 }),
+            user: socket.userID,
+            date: Date.now()
+        }
+        let messages = getMessages()
+        messages.push(data)
+        loadMessages(messages)
+        data.user = getUsers()[socket.userID]
+        io.emit('chat:addMessage', data)
     })
 })
